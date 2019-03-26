98403d6ceec3702058f94e9f16a982c8c9febb58
sawDandelion <1198271500@qq.com>
2019-03-26 15:21:02 +0800

    删除多余代码

--- src/children-component.vue
+++ src/children-component.vue
@@ -12,11 +12,6 @@
 
             </div>
         </div> -->
-        <!-- <div>
-            <span @click="toSkip('/foo')">to todo1</span>
-            <span @click="toSkip('/about')">to todo2</span>
-        </div>
-        <router-view /> -->
         <div>
             <span @click="toSkip('/foo')">to todo1</span>
             <span @click="toSkip('/about')">to todo2</span>
